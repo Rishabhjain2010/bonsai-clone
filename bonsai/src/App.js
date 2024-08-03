9e582651261531b49ec9834df304a537f7468a49
Chirag Gaur <147283656+ChiragGaur13@users.noreply.github.com>
2024-08-03 16:15:30 +0530

    Landing page developed (#3)
    * Developed home page
    
    * made design Responsive
    
    ---------
    
    Co-authored-by: Rishabh Jain <83902727+Rishabhjain2010@users.noreply.github.com>

--- bonsai/src/App.js
+++ bonsai/src/App.js
@@ -1,17 +1,23 @@
+
+import Dashboard from "./components/Dashboard"
+
 import React from 'react';
 import Feature from './components/features';
 import './App.css';
 import Rating from './components/ratings';
 
+
 const App = () => {
   return (
+
   
     <div className='page'>
+    <Dashboard />
     <Feature/>
     <Rating/>
 
     </div>
-    
+
   );
 }
 
