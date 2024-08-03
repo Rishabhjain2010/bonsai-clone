c8173d6800d06f40700d6aaf9ca10a8f10c9fb96
rishabhjian2010 <jainrishabh.201@gmail.com>
2024-08-03 15:38:36 +0530

    features designed

--- bonsai/src/App.js
+++ bonsai/src/App.js
@@ -1,37 +1,16 @@
 import React from 'react';
 import Feature from './components/features';
 import './App.css';
-
-
-//  const features = [
-//   {
-//     title: 'Deliver projects on-time & on-budget',
-//     description: '✓ Assign, prioritize & track tasks,✓ Track your budget across projects. ✓ Log your team\'s time in-app or in timesheets.',
-//     buttonText: 'Get Started',
-//     imgSrc: '/assets/consultingpartner.png',  
-//     imgAlt: 'Project Management Image'
-//   },
-//   {
-//     title: 'Title 2',
-//     description: 'Description 2', 
-//     buttonText: 'Button Text 2',
-//     imgSrc: '/assets/profitability.png', 
-//     imgAlt: 'Alt Text 2'
-//   },
-//   {
-//     title: 'Title 3',
-//     description: 'Description 3',
-//     buttonText: 'Button Text 3',
-//     imgSrc: '/assets/projectpepiline.png',  
-//     imgAlt: 'Alt Text 3'
-//   }
-// ];
+import Rating from './components/ratings';
 
 const App = () => {
   return (
-    <div className="App">
+  
+    <div className='page'>
     <Feature/>
+    <Rating/>
     </div>
+    
   );
 }
 
--- bonsai/src/components/features.js
+++ bonsai/src/components/features.js
@@ -8,62 +8,71 @@ return (
 
         <div className={styles.main}>
             <div className={styles.feature}>
+                 <div className={styles.imageend}>
+                    <img src="/assets/consultingpartner.png"alt= "https://cdn.prod.website-files.com/637e5892fb4b6db88a62cc0a/6666d70e419ec905de6e73f2_PMs-Projects%20(5)%20(1)-p-800.png"/>
+                </div>
                 <div className={styles.text}>
                     <h2> Deliver projects on-time & on-budget </h2>
                         <div className={styles.flexp}  >
-                            <p> ✓ Monitor key agency KPIs in real time. </p>
-                            <p> ✓ Monitor key agency KPIs in real time. </p>
+                            <p> ✓ Assign, prioritize & track tasks. </p>
+                            <p> ✓ Track your budget across projects. </p>
+                            <p> ✓ Log your team's time in-app or in timesheets. </p>
+                        </div>
+
+                        <div className={styles.button}>
+                            <button className={styles.button}>Get Started</button>
+                        </div>                
+                </div>
+                
+            </div>
+        </div>
+
+
+        <div className={styles.main}>
+            <div className={styles.feature}>
+                <div className={styles.text}>
+                    <h2> Track revenue & boost profitability </h2>
+                        <div className={styles.flexp}  >
+                            <p> ✓ Monitor key agency KPIs in real time.</p>
+                            <p> ✓ Optimize performance & increase margins. </p>
                             <p> ✓ Plan ahead for future growth. </p>
                         </div>
                     <div className={styles.button}>
-                    <a href="" className='utton'>Get Started</a>
+                    <button className={styles.button}>Get Started</button>
                     </div>
                 </div>
-                <div className={styles.imageend}>
-                    <img src="/assets/consultingpartner.png"alt= "https://cdn.prod.website-files.com/637e5892fb4b6db88a62cc0a/6666d70e419ec905de6e73f2_PMs-Projects%20(5)%20(1)-p-800.png"/>
-                </div>
-            </div>
-        </div>
-
-
-
-        <div className={styles.main}>
-            <div className={styles.features}>
-                <div className={styles.image}>
-                    <img src="/assets/profitability.png"alt= "https://cdn.prod.website-files.com/637e5892fb4b6db88a62cc0a/66aa4d9004841d5719a54841_Profitability%20Forecast-p-800.png"/>
-                </div>
-                <div className={styles.text}>
-                    <h2> Deliver projects on-time & on-budget </h2>
-                    <p> ✓ Monitor key agency KPIs in real time. </p>
-                    <p> ✓ Monitor key agency KPIs in real time. </p>
-                    <p> ✓ Plan ahead for future growth. </p>
-                    <div className={styles.button}>
-                    <a href="" className='button'>Get Started</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <div className={styles.main}>
-            <div className={styles.features}>
-                <div className={styles.text}>
-                    <h2> Deliver projects on-time & on-budget </h2>
-                    <p> ✓ Monitor key agency KPIs in real time. </p>
-                    <p> ✓ Monitor key agency KPIs in real time. </p>
-                    <p> ✓ Plan ahead for future growth. </p>
-                    <div className={styles.button}>
-                    <a href="" className='button'>Get Started</a>
-                    </div>
-                </div>
-                <div className={styles.image}>
+                <div className={styles.imagestart}>
                     <img src="/assets/projectpipeline.png"alt= "https://cdn.prod.website-files.com/637e5892fb4b6db88a62cc0a/6666d70e419ec905de6e73f2_PMs-Projects%20(5)%20(1)-p-800.png"/>
                 </div>
+               
             </div>
         </div>
+
+        <div className={styles.main}>
+            <div className={styles.feature}>
+                <div className={styles.imageend}>
+                    <img src="/assets/projectpipeline.png"alt= "https://cdn.prod.website-files.com/637e5892fb4b6db88a62cc0a/6666d70e419ec905de6e73f2_PMs-Projects%20(5)%20(1)-p-800.png"/>
+                </div>
+                <div className={styles.text}>
+                    <h2> Manage clients & get paid </h2>
+                        <div className={styles.flexp}  >
+                            <p> ✓ Centralize client info in your CRM. </p>
+                            <p> ✓ Send invoices & get paid. </p>
+                            <p> ✓ Automate all your workflows. </p>
+                        </div>
+                    <div className={styles.button}>
+                    <button className={styles.button}>Get Started</button>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
 
 
 </section>
+
+
 );
 
 };
